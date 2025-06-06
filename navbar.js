e1e784670c3f3047377a4f0c6a91bfde049720d3
AngryHtml <123128711+angryhtml@users.noreply.github.com>
2025-06-06 13:41:04 +0200

    change navbar behavior

--- navbar.js
+++ navbar.js
@@ -1,9 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
     const burger = document.querySelector('.navbar-burger');
     const menu = document.querySelector('#navbarMenu');
+    const menuItems = menu.querySelectorAll('.navbar-item[href^="#"]');
+
 
     burger.addEventListener('click', () => {
       burger.classList.toggle('is-active');
       menu.classList.toggle('is-active');
     });
+
+    menuItems.forEach(item => {
+      item.addEventListener('click', () => {
+        burger.classList.remove('is-active');
+        menu.classList.remove('is-active');
+      })
+    })
   });
+
+
